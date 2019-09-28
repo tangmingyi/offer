1a9dc150498500c0861c5d41a942c806a5714696
tmy_notebook <zb19851222@qq.com>
2019-09-28 15:07:02 +0800

    25 递归

--- 25.cpp
+++ 25.cpp
@@ -1,6 +1,3 @@
-//
-// Created by zb198 on 2019/9/28.
-//
 
 struct ListNode {
 	int val;
@@ -10,44 +7,24 @@ struct ListNode {
 	}
 };
 class Solution {
-private:
-    ListNode* pHead = nullptr;
-    ListNode* pNode = nullptr;
-    void updata(ListNode** pHead1, ListNode** pHead2){
-        if((*pHead1)->val<(*pHead2)->val){
-            if(pNode== nullptr){
-                pNode = *pHead1;
-                pHead = pNode;
-            } else {
-                pNode->next = *pHead1;
-            }
-            *pHead1 = (*pHead1)->next;
-        } else{
-            if(pNode== nullptr){
-                pNode = *pHead2;
-                pHead = pNode;
-            } else {
-                pNode->next = *pHead2;
-            }
-            *pHead2 = (*pHead2)->next;
-        }
-        pNode = pNode->next;
-    }
 public:
     ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
     {
-        if(pHead1== nullptr and pHead2== nullptr){
-            return nullptr;
-        }
-        while (pHead1 != nullptr || pHead2!= nullptr){
-            updata(&pHead1,&pHead2);
-        }
         if(pHead1== nullptr){
-            pNode->next = pHead2;
-        } else{
-            pNode->next = pHead1;
+            return pHead2;
         }
-        return pHead;
+        if(pHead2== nullptr){
+            return pHead1;
+        }
+        ListNode* mergerPhead = nullptr;
+        if(pHead1->val<pHead2->val){
+            mergerPhead = pHead1;
+            mergerPhead->next = Merge(pHead1->next,pHead2);
+        } else{
+            mergerPhead = pHead2;
+            mergerPhead->next = Merge(pHead1,pHead2->next);
+        }
+        return mergerPhead;
+
     }
 };
-
--- 25.py
+++ 25.py
@@ -5,37 +5,19 @@ class ListNode:
         self.next = None
 class Solution:
     # 返回合并后列表
-    def init(self,pHead1:ListNode, pHead2:ListNode):
-        self.pHead1:ListNode = pHead1
-        self.pHead2:ListNode = pHead2
-        if(self.pHead1.val<self.pHead2.val):
-            self.pHead = self.pHead1
-            self.pHead1 = self.pHead1.next
+    def Merge(self, pHead1:ListNode, pHead2:ListNode)->ListNode:
+        if(pHead1==None):
+            return pHead2
+        if(pHead2==None):
+            return pHead1
+        mergePhead = None
+        if(pHead1.val<pHead2.val):
+            mergePhead = pHead1
+            mergePhead.next = self.Merge(pHead1.next,pHead2)
         else:
-            self.pHead = self.pHead2
-            self.pHead2 = self.pHead2.next
-        self.pNode = self.pHead
+            mergePhead = pHead2
+            mergePhead.next = self.Merge(pHead1,pHead2.next)
+        return mergePhead
 
-    def core(self):
-        if(self.pHead1.val<self.pHead2.val):
-            self.pNode.next = self.pHead1
-            self.pHead1 = self.pHead1.next
-            self.pNode = self.pNode.next
-        else:
-            self.pNode.next = self.pHead2
-            self.pNode = self.pNode.next
-            self.pHead2 = self.pHead2.next
-
-    def Merge(self, pHead1:ListNode, pHead2:ListNode):
-        if(pHead1==None and pHead2==None):
-            return None
-        self.init(pHead1,pHead2)
-        while(self.pHead1!=None or self.pHead2!=None):
-            self.core()
-        if(self.pHead1==None):
-            self.pNode.next = self.pHead2
-        else:
-            self.pNode.next = self.pHead1
-        return self.pHead
 
 # write code here
